ab371475aded0ce7871befaaac6296f2149ed6c9
ImmanuelTan <immanuel1828@gmail.com>
2022-06-01 16:57:53 +0700

    number

---
+++ lib/number.dart
@@ -0,0 +1,8 @@
+// type data integer
+var number = 2020;
+var hex = 0xDEADBEEF;
+
+// type data double
+var decimal = 1.2;
+var pi = 3.14;1
+double withoutDecimal = 7; // Sama dengan double withoutDecimal = 7.0
